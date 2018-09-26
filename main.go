696e9f21c2eee713b7702df03814aeb555829a0f
Stefan Majewsky <stefan.majewsky@sap.com>
2018-09-26 15:50:42 +0200

    simplify example

--- main.go
+++ main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/identity/v3/tokens"
 	"github.com/gophercloud/gophercloud/pagination"
+	"github.com/gophercloud/utils/openstack/clientconfig"
 
 	"github.com/sapcc/gophercloud-limes/limes"
 	"github.com/sapcc/gophercloud-limes/limes/v1/projects"
 )
 
 func main() {
-	provider := NewAuthenticatedClient()
+	provider, err := clientconfig.AuthenticatedClient(nil)
+	if err != nil {
+		log.Fatalf("could not initialize openstack client: %v", err)
+	}
 	limesClient := NewLimes(provider)
 	identityClient := NewIdentity(provider)
 
@@ -37,34 +40,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("couldn't get projects: %v", err)
 	}
-}
-
-func NewAuthenticatedClient() *gophercloud.ProviderClient {
-	authOpts := &tokens.AuthOptions{
-		IdentityEndpoint: os.Getenv("OS_AUTH_URL"),
-		Username:         os.Getenv("OS_USERNAME"),
-		Password:         os.Getenv("OS_PASSWORD"),
-		DomainName:       os.Getenv("OS_USER_DOMAIN_NAME"),
-		AllowReauth:      true,
-		Scope: tokens.Scope{
-			ProjectName: os.Getenv("OS_PROJECT_NAME"),
-			DomainName:  os.Getenv("OS_PROJECT_DOMAIN_NAME"),
-		},
-	}
-
-	provider, err := openstack.NewClient(os.Getenv("OS_AUTH_URL"))
-	if err != nil {
-		log.Fatalf("could not initialize openstack client: %v", err)
-	}
-
-	provider.UseTokenLock()
-
-	err = openstack.AuthenticateV3(provider, authOpts, gophercloud.EndpointOpts{})
-	if err != nil {
-		log.Fatalf("could not authenticat provider client: %v", err)
-	}
-
-	return provider
 }
 
 func NewIdentity(provider *gophercloud.ProviderClient) *gophercloud.ServiceClient {
