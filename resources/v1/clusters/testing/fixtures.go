e8092d56d3754a220a2b058d1739d146774ee62b
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2022-03-03 01:18:10 +0100

    resources/v1/clusters: move fixtures into individual files

--- resources/v1/clusters/testing/fixtures.go
+++ resources/v1/clusters/testing/fixtures.go
@@ -2,101 +2,14 @@ package testing
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 	"testing"
 
 	th "github.com/gophercloud/gophercloud/testhelper"
 	fake "github.com/gophercloud/gophercloud/testhelper/client"
 )
-
-var clusterJSON = `
-	{
-		"cluster": {
-			"id": "pakistan",
-			"services": [
-				{
-					"type": "shared",
-					"area": "shared",
-					"resources": [
-						{
-							"name": "stuff",
-							"capacity": 10,
-							"domains_quota": 5,
-							"unit": "B",
-							"usage": 2
-						},
-						{
-							"name": "things",
-							"capacity": 10,
-							"domains_quota": 5,
-							"usage": 2
-						}
-					],
-					"max_scraped_at": 33,
-					"min_scraped_at": 33
-				},
-				{
-					"shared": true,
-					"type": "unshared",
-					"area": "contradiction",
-					"resources": [
-						{
-							"name": "stuff",
-							"capacity": 10,
-							"comment": "tasty tests are so tasty",
-							"domains_quota": 5,
-							"unit": "B",
-							"usage": 2
-						},
-						{
-							"name": "things",
-							"capacity": 10,
-							"domains_quota": 5,
-							"usage": 2
-						}
-					],
-					"max_scraped_at": 33,
-					"min_scraped_at": 33
-				}
-			],
-			"max_scraped_at": 22,
-			"min_scraped_at": 22
-		}
-	}
-`
-
-var clusterFilteredJSON = `
-	{
-		"cluster": {
-			"id": "pakistan",
-			"services": [
-				{
-					"shared": true,
-					"type": "unshared",
-					"area": "contradiction",
-					"resources": [
-						{
-							"name": "stuff",
-							"capacity": 10,
-							"comment": "tasty tests are so tasty",
-							"domains_quota": 4,
-							"unit": "B",
-							"usage": 2,
-							"subcapacities": [
-								{ "hypervisor": "cluster-1", "cores": 200 },
-								{ "hypervisor": "cluster-2", "cores": 800 }
-							]
-						}
-					],
-					"max_scraped_at": 33,
-					"min_scraped_at": 33
-				}
-			],
-			"max_scraped_at": 22,
-			"min_scraped_at": 22
-		}
-	}
-`
 
 // HandleGetClusterSuccessfully creates an HTTP handler at `/v1/clusters/:cluster_id` on the
 // test handler mux that responds with a single cluster.
@@ -108,11 +21,14 @@ func HandleGetClusterSuccessfully(t *testing.T) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
+		fixtureName := "get.json"
 		if (r.URL.Query().Get("service") == "unshared" || r.URL.Query().Get("area") == "contradiction") &&
 			r.URL.Query().Get("resource") == "stuff" && r.URL.Query().Get("detail") != "" {
-			fmt.Fprintf(w, clusterFilteredJSON)
+			fixtureName = "get-filtered.json"
 		}
 
-		fmt.Fprintf(w, clusterJSON)
+		jsonBytes, err := ioutil.ReadFile(filepath.Join("fixtures", fixtureName))
+		th.AssertNoErr(t, err)
+		fmt.Fprint(w, string(jsonBytes))
 	})
 }
