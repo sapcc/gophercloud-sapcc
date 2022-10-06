858939a9bd8222a312531749f402ca47616af3b5
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2022-10-06 14:14:28 +0200

    resources/v1/clusters: remove obsolete method
    We do not need `ExtractClusters()` as Limes no longer supports multiple clusters.

--- resources/v1/clusters/results.go
+++ resources/v1/clusters/results.go
@@ -19,20 +19,10 @@ import (
 	"github.com/sapcc/go-api-declarations/limes"
 )
 
-// CommonResult is the result of a Get/List operation. Call its appropriate
-// Extract method to interpret it as a Cluster or a slice of Clusters.
+// CommonResult is the result of a Get operation. Call its appropriate
+// Extract method to interpret it as a Cluster.
 type CommonResult struct {
 	gophercloud.Result
-}
-
-// ExtractClusters interprets a CommonResult as a slice of Clusters.
-func (r CommonResult) ExtractClusters() ([]limes.ClusterReport, error) {
-	var s struct {
-		Clusters []limes.ClusterReport `json:"clusters"`
-	}
-
-	err := r.ExtractInto(&s)
-	return s.Clusters, err
 }
 
 // Extract interprets a CommonResult as a Cluster.
