28d8056f7b328861c0433133b2ef7478e07643ef
Ivo Gosemann <ivo.gosemann@sap.com>
2023-04-26 14:05:17 +0200

    [billing] adds options to projects list

--- billing/masterdata/projects/requests.go
+++ billing/masterdata/projects/requests.go
@@ -16,15 +16,55 @@ package projects
 
 import (
 	"encoding/json"
+	"net/url"
+	"time"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/pagination"
 )
 
+// ListOpts is a structure that holds options for listing project masterdata.
+type ListOpts struct {
+	CheckCOValidity bool      `q:"checkCOValidity"`
+	ExcludeDeleted  bool      `q:"excludeDeleted"`
+	From            time.Time `q:"-"`
+}
+
+// ToProjectListQuery formats a ListOpts into a query string.
+func (opts ListOpts) ToProjectListQuery() (string, error) {
+	q, err := gophercloud.BuildQueryString(opts)
+	if err != nil {
+		return "", err
+	}
+	params := q.Query()
+
+	if opts.From != (time.Time{}) {
+		params.Add("from", opts.From.Format(gophercloud.RFC3339MilliNoZ))
+	}
+
+	q = &url.URL{RawQuery: params.Encode()}
+
+	return q.String(), nil
+}
+
 // List returns a Pager which allows you to iterate over a collection of
 // projects.
 func List(c *gophercloud.ServiceClient) pagination.Pager {
-	return pagination.NewPager(c, listURL(c), func(r pagination.PageResult) pagination.Page {
+	return ListWithOpts(c, ListOpts{})
+}
+
+// List returns a Pager which allows you to iterate over a collection of
+// projects optionally filtered by ListOpts
+func ListWithOpts(c *gophercloud.ServiceClient, opts ListOpts) pagination.Pager {
+	serviceURL := listURL(c)
+	if opts != (ListOpts{}) {
+		query, err := opts.ToProjectListQuery()
+		if err != nil {
+			return pagination.Pager{Err: err}
+		}
+		serviceURL += query
+	}
+	return pagination.NewPager(c, serviceURL, func(r pagination.PageResult) pagination.Page {
 		return ProjectPage{pagination.SinglePageBase(r)}
 	})
 }
