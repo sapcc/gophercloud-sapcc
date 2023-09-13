354a880e11cf486262c9c71c30872fc44e62db66
IvoGoman <ivo.gosemann@sap.com>
2023-09-13 14:38:25 +0200

    feat(issue47): add metis client and get/list projects (#51)

---
+++ metis/v1/identity/projects/testing/requests_test.go
@@ -0,0 +1,179 @@
+// Copyright 2023 SAP SE
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testing
+
+import (
+	"testing"
+
+	th "github.com/gophercloud/gophercloud/testhelper"
+	fakeclient "github.com/gophercloud/gophercloud/testhelper/client"
+
+	"github.com/sapcc/gophercloud-sapcc/metis/v1/identity/projects"
+)
+
+func TestGetProject(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandleGetProjectSuccessfully(t)
+
+	actual, err := projects.Get(fakeclient.ServiceClient(), "project-1").Extract()
+	th.AssertNoErr(t, err)
+
+	expected := &projects.Project{
+		Name:        "project1",
+		UUID:        "project-1",
+		Description: "project1 descr",
+		DomainName:  "domain1",
+		DomainUUID:  "domain-1",
+		CBRMasterdata: projects.CBRMasterdata{
+			CostObjectName:                  "my-cost-object",
+			CostObjectType:                  "IO",
+			CostObjectInherited:             true,
+			BusinessCriticality:             "test",
+			RevenueRelevance:                "true",
+			NumberOfEndusers:                0,
+			PrimaryContactUserID:            "i001337",
+			PrimaryContactEmail:             "max.mustermann@sample.com",
+			OperatorUserID:                  "i001337",
+			OperatorEmail:                   "max.mustermann@sample.com",
+			InventoryRoleUserID:             "i001337",
+			InventoryRoleEmail:              "max.mustermann@sample.com",
+			InfrastructureCoordinatorUserID: "i001337",
+			InfrastructureCoordinatorEmail:  "max.mustermann@sample.com",
+			ExternalCertifications: projects.ExternalCertifications{
+				ISO:  false,
+				PCI:  true,
+				SOC1: false,
+				SOC2: false,
+				C5:   false,
+				SOX:  false,
+			},
+			GPUEnabled:                   false,
+			ContainsPIIDPPHR:             true,
+			ContainsExternalCustomerData: true,
+		},
+		Users: []projects.User{
+			{
+				UUID:        "1234abcd",
+				Name:        "Max Mustermann",
+				Email:       "max.mustermann@sample.com",
+				Description: "muster",
+			},
+		},
+	}
+
+	th.CheckDeepEquals(t, expected, actual)
+}
+
+func TestListProjects(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+	HandleListProjectsSuccessfully(t)
+	opts := projects.ListOpts{
+		Limit: 1,
+	}
+
+	p, err := projects.List(fakeclient.ServiceClient(), opts).AllPages()
+	th.AssertNoErr(t, err)
+	actual, err := projects.ExtractProjects(p)
+	th.AssertNoErr(t, err)
+
+	expected := []projects.Project{
+		{
+			Name:        "project1",
+			UUID:        "project-1",
+			Description: "project1 descr",
+			DomainName:  "domain1",
+			DomainUUID:  "domain-1",
+			CBRMasterdata: projects.CBRMasterdata{
+				CostObjectName:                  "my-cost-object",
+				CostObjectType:                  "IO",
+				CostObjectInherited:             true,
+				BusinessCriticality:             "test",
+				RevenueRelevance:                "true",
+				NumberOfEndusers:                0,
+				PrimaryContactUserID:            "i001337",
+				PrimaryContactEmail:             "max.mustermann@sample.com",
+				OperatorUserID:                  "i001337",
+				OperatorEmail:                   "max.mustermann@sample.com",
+				InventoryRoleUserID:             "i001337",
+				InventoryRoleEmail:              "max.mustermann@sample.com",
+				InfrastructureCoordinatorUserID: "i001337",
+				InfrastructureCoordinatorEmail:  "max.mustermann@sample.com",
+				ExternalCertifications: projects.ExternalCertifications{
+					ISO:  false,
+					PCI:  true,
+					SOC1: false,
+					SOC2: false,
+					C5:   false,
+					SOX:  false,
+				},
+				GPUEnabled:                   false,
+				ContainsPIIDPPHR:             true,
+				ContainsExternalCustomerData: true,
+			},
+			Users: []projects.User{
+				{
+					UUID:        "1234abcd",
+					Name:        "Max Mustermann",
+					Email:       "max.mustermann@sample.com",
+					Description: "muster",
+				},
+			},
+		},
+		{
+			Name:        "project2",
+			UUID:        "project-2",
+			Description: "project2 descr",
+			DomainName:  "domain2",
+			DomainUUID:  "domain-2",
+			CBRMasterdata: projects.CBRMasterdata{
+				CostObjectName:                  "my-cost-object",
+				CostObjectType:                  "IO",
+				CostObjectInherited:             false,
+				BusinessCriticality:             "test",
+				RevenueRelevance:                "true",
+				NumberOfEndusers:                0,
+				PrimaryContactUserID:            "i001337",
+				PrimaryContactEmail:             "max.mustermann@sample.com",
+				OperatorUserID:                  "i001337",
+				OperatorEmail:                   "max.mustermann@sample.com",
+				InventoryRoleUserID:             "i001337",
+				InventoryRoleEmail:              "max.mustermann@sample.com",
+				InfrastructureCoordinatorUserID: "i001337",
+				InfrastructureCoordinatorEmail:  "max.mustermann@sample.com",
+				ExternalCertifications: projects.ExternalCertifications{
+					ISO:  false,
+					PCI:  false,
+					SOC1: false,
+					SOC2: false,
+					C5:   false,
+					SOX:  false,
+				},
+				GPUEnabled:                   false,
+				ContainsPIIDPPHR:             true,
+				ContainsExternalCustomerData: true,
+			},
+			Users: []projects.User{
+				{
+					UUID: "abcd1234",
+					Name: "TU1234",
+				},
+			},
+		},
+	}
+
+	th.CheckDeepEquals(t, expected, actual)
+}
---
+++ metis/v1/pagination.go
@@ -0,0 +1,78 @@
+// Copyright 2023 SAP SE
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import "github.com/gophercloud/gophercloud/pagination"
+
+// CommonPage is the common base type for all pagination pages used by Metis V1 endpoints.
+type CommonPage struct {
+	pagination.LinkedPageBase
+}
+
+// IsEmpty determines whether or not a page of projects contains any results.
+func (p CommonPage) IsEmpty() (bool, error) {
+	items, err := extractItems(p)
+	if err != nil {
+		return true, err
+	}
+	return len(items) == 0, nil
+}
+
+// NextPageURL extracts the "next" link from the response.
+func (p CommonPage) NextPageURL() (string, error) {
+	var s struct {
+		Next string `json:"next"`
+	}
+
+	err := p.ExtractInto(&s)
+	if err != nil {
+		return "", err
+	}
+	return s.Next, err
+}
+
+func extractItems(p pagination.Page) ([]any, error) {
+	var s struct {
+		Items []any `json:"items"`
+	}
+	if err := (p.(CommonPage)).ExtractInto(&s); err != nil {
+		return []any{}, err
+	}
+	return s.Items, nil
+}
+
+// CreatePage is used to use pagination.NewPager with any of the Metis V1 endpoints.
+func CreatePage() func(pagination.PageResult) pagination.Page {
+	return func(r pagination.PageResult) pagination.Page {
+		// The structure of the Metis API response has the list of projects not on the top level, but nested in the "data" field.
+		// pagination.Pager does not work with this structure, so we extract the list of projects and the next page URL.
+		// The body is then replaced with a map containing the list of projects and the next page URL, which is then
+		// compatible with the pagination.
+		var source struct {
+			Data struct {
+				Items []any  `json:"items"`
+				Next  string `json:"nextLink"`
+			} `json:"data"`
+		}
+		if err := r.ExtractInto(&source); err != nil {
+			return nil
+		}
+		newBody := make(map[string]any, 2)
+		newBody["items"] = source.Data.Items
+		newBody["next"] = source.Data.Next
+		r.Body = newBody
+		return CommonPage{pagination.LinkedPageBase{PageResult: r}}
+	}
+}
